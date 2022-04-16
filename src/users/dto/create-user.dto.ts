79ff4f3471c95dc954665e646db67215e6f955f6
Prateek Anand <bizprat@gmail.com>
2022-04-16 07:17:49 +0530

    Create User DTO with class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsNotEmpty()
+  @IsString()
+  @IsNotEmpty({ message: '$property is required' })
+  @IsEmail({}, { message: '$value is not a valid $property' })
   email: string;
 
+  @IsString({ message: '$property is not a string type' })
   @IsNotEmpty()
+  @MinLength(6, {
+    message: '$property must have atleast $constraint1 characters',
+  })
   password: string;
 }
