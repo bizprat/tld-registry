f80277dfde9d26e3e1bcc9d112f422b0e99bcc43
Prateek Anand <bizprat@gmail.com>
2022-04-19 01:31:22 +0530

    Updated user.dto.ts

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,6 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { Expose, Exclude } from 'class-transformer';
-import { UserInterface } from '../user.interface';
 import { AuthDto } from './auth.dto';
 
 // TODO: Use either Expose or Exclude
@@ -8,6 +7,6 @@ export class UserDto extends PartialType(AuthDto) {
   @Expose()
   id?: number;
 
-  // @Exclude()
+  @Exclude()
   password?: string;
 }
