13ddf0dedb5152a3c35b6ef41157e955591e4f4c
Prateek Anand <bizprat@gmail.com>
2022-04-23 16:31:06 +0530

    Added: Current user decorator

--- src/decorators/current-user.decorator.ts
+++ src/decorators/current-user.decorator.ts
@@ -3,7 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: never, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
-    console.log(req.session.userId);
-    return req.session.userId;
+    return req.currentUser;
   },
 );
